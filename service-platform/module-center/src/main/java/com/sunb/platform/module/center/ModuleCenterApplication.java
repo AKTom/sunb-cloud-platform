c334c3476782bbfd1f3ecdbc2694f8099277c064
SUNB <512796048@qq.com>
2018-04-19 18:15:57 +0800

    spring-cloud

--- service-platform/module-center/src/main/java/com/sunb/platform/module/center/ModuleCenterApplication.java
+++ service-platform/module-center/src/main/java/com/sunb/platform/module/center/ModuleCenterApplication.java
@@ -1,16 +1,16 @@
 package com.sunb.platform.module.center;
 
 
+import com.sunb.base.service.framework.annotation.CloudPlatformServiceApplication;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.ComponentScan;
 
 @EnableFeignClients("com.sunb")
 @ComponentScan(basePackages = "com.sunb")
 @MapperScan("com.sunb.platform.module.center.dao")
-@SpringBootApplication
+@CloudPlatformServiceApplication
 public class ModuleCenterApplication {
     public static void main(String [] args){
         SpringApplication.run(ModuleCenterApplication.class, args);
