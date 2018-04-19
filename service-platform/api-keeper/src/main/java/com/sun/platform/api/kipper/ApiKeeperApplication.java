57d55817e3dc6d4cf2da34eef4e02a6857122d56
SUNB <512796048@qq.com>
2018-04-19 13:24:18 +0800

    spring-cloud

--- service-platform/api-keeper/src/main/java/com/sun/platform/api/kipper/ApiKeeperApplication.java
+++ service-platform/api-keeper/src/main/java/com/sun/platform/api/kipper/ApiKeeperApplication.java
@@ -8,7 +8,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 
 /**
  * Description: 网关服务
